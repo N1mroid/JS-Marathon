262b5eee6928134130e20e7cea1c9e17b936e38a
Enkarto <roman_emperor5@mail.ru>
2020-08-18 19:24:53 +0500

    Construction complete.

--- main.js
+++ main.js
@@ -12,3 +12,9 @@ console.log(noValue);
 // 5. Object
 // 6. String
 // 7. Number
+
+// Dynamic types of variables and type conversion
+// String(5) -> converts to a number to string
+// console.log(concat + {}) -> [object Object]
+// Number('10') -> converts a string into number or (+'10')
+// console.log(!!'asd') -> converts a string into true. ! false
