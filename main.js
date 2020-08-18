5ad8661bf9e8ead43b8ee335457297c94741541b
Enkarto <roman_emperor5@mail.ru>
2020-08-18 18:18:08 +0500

    JS Data Types added

---
+++ main.js
@@ -0,0 +1,14 @@
+const notANumber = 'sad' * 8; // NaN
+console.log(notANumber);
+
+let noValue; // undefined
+console.log(noValue);
+
+// Data types
+// 1. Boolean
+// 2. Symbol
+// 3. BigInt
+// 4. null && undefined
+// 5. Object
+// 6. String
+// 7. Number
